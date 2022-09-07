692a89816e621ad198ea65a5940cb9cd0a18e447
giovandrofabio <giovandrofabiosantos@hotmail.com>
2022-09-06 22:15:27 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from './components/NotificationButton/index';
+
 function App() {
   return(
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
